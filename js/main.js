bca0ca5cd84519fe79ec08b6672df249add3ce01
Xushnora <xushnoranishonova@gmail.com>
2022-02-12 17:58:02 +0500

    changed animation

--- js/main.js
+++ js/main.js
@@ -24,13 +24,13 @@ elMenuBtn.addEventListener('click', function () {
 
 // animation welcome
 
-var car = ["Welcome"];
+var car = ["Xushnora"];
 var i=0;
 function anime(){
 $('#text').hide();
 $('#text').html(car[i]);
-$('#text').delay(100).show(500);
-$('#text').delay(500).hide(500);
+$('#text').delay(300).show(500);
+$('#text').delay(1000).hide(500);
 i=i+1
 if(i==3){i=0;}
 anime();
@@ -38,3 +38,9 @@ anime();
 anime();
 
 // animation the end
+
+jQuery(document).ready(function($){
+    $('.main__title').typeIt({
+        content : 'I am a Web Developer'
+    });
+});
