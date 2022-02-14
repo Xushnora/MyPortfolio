08e0fdbf51846bfec2b4b223c8185ba586fbc78d
Xushnora <xushnoranishonova@gmail.com>
2022-02-14 13:35:54 +0500

    changeds

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 let elMenuBtn = document.getElementById('menuBtn');
 let elModal = document.getElementById('navModal');
 let elClose = document.getElementById('closeBtn');
-let elIcon = document.getElementById('icon')
-
-
+let elIcon = document.getElementById('icon');
 
 let count = 0;
 elMenuBtn.addEventListener('click', function () {
@@ -18,6 +16,20 @@ elMenuBtn.addEventListener('click', function () {
     }
 });
 
+// MODAL PORTFOLIO
+
+let elProject = document.getElementById('portContent');
+let elModalcofe = document.getElementById('cofeeModal');
+let elClosecofe = document.getElementById('closeBtncofe');
+
+elProject.addEventListener('click', function () {
+    elModalcofe.classList.add('test');
+});
+
+elClosecofe.addEventListener('click', function () {
+    elModalcofe.classList.remove('test');
+})
+
 
 
 
@@ -25,22 +37,25 @@ elMenuBtn.addEventListener('click', function () {
 // animation welcome
 
 var car = ["Xushnora"];
-var i=0;
-function anime(){
-$('#text').hide();
-$('#text').html(car[i]);
-$('#text').delay(300).show(500);
-$('#text').delay(1000).hide(500);
-i=i+1
-if(i==3){i=0;}
-anime();
+var i = 0;
+
+function anime() {
+    $('#text').hide();
+    $('#text').html(car[i]);
+    $('#text').delay(300).show(500);
+    $('#text').delay(1000).hide(500);
+    i = i + 1
+    if (i == 3) {
+        i = 0;
+    }
+    anime();
 }
 anime();
 
 // animation the end
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
     $('.main__title').typeIt({
-        content : 'I am a Web Developer'
+        content: 'I am a Web Developer'
     });
 });
